6b93e57b66216ad5567610baaad9c9059b99b49d
utahta <labs.ninxit@gmail.com>
2016-01-11 23:52:44 +0900

    add

---
+++ ch2/tempconv0.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
+
+func main() {
+	f := CToF(37.0)
+	fmt.Println(f)
+	fmt.Println(FToC(f))
+}
