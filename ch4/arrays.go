da289f20bde7091233dc61a9c502d54f7849c4e0
utahta <labs.ninxit@gmail.com>
2016-01-14 00:56:25 +0900

    add ch4

---
+++ ch4/arrays.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//t := [5]int{1, 2, 3, 4.2, 5}
+	//fmt.Println(t)
+
+	var a [3]int
+	fmt.Println(a[0])
+	fmt.Println(a[len(a)-1])
+	//fmt.Println(a[len(a)])
+	for i, v := range a {
+		fmt.Printf("%d %d\n", i, v)
+	}
+
+	for _, v := range a {
+		fmt.Printf("%d\n", v)
+	}
+
+	var q [3]int = [3]int{10, 20, 30}
+	fmt.Println(q)
+
+	r := [3]int{10, 20}
+	fmt.Println(r)
+
+	r = [3]int{3, 2, 1}
+	fmt.Println(r)
+
+	q2 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(q2)
+
+	q3 := [...]int{99: -1}
+	fmt.Println(len(q3))
+
+	a1 := [2]int{1, 2}
+	a2 := [...]int{1, 2}
+	a3 := [2]int{1, 3}
+	fmt.Println(a1 == a2, a1 == a3)
+	//a4 := [3]int{1, 2}
+	//fmt.Println(a1 == a4)
+}
